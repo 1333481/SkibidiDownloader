cd031c454fcc20db68d77f3d1f12caa9b5c19026
Jesse Stoltz <jessestoltz07@gmail.com>
2024-06-16 14:05:49 -0500

    Fix updater

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ else:
 def update_checker():
     system = platform.system()
     if system == 'Windows':
-        installed_ver = Version("1.0.0")
+        installed_ver = Version("1.0.2")
         response = requests.get("https://api.github.com/repos/1333481/SkibidiDownloader/releases/latest")
         release_info = response.json()
         getver = release_info['tag_name']
@@ -49,7 +49,7 @@ def update_checker():
                             f.write(chunk)
                 try:
                     subprocess.Popen([local_filename])
-                    os._exit()
+                    os._exit(0)
                 except subprocess.CalledProcessError as e:
                     messagebox.showerror("Error!", "Error opening EXE!")
 
